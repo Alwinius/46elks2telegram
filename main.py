82b0258247d15668fb1847a3a2e3de1dd574ce66
Alwin <alwin+github@alwin.me>
2023-11-26 11:27:05 +0100

    Downgrade telegram framework because of async issues

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bottle import request, post, run, HTTPResponse
 from telegram.constants import ParseMode
+from telegram import Bot
 from telegram.ext import ApplicationBuilder
 import configparser
 
@@ -9,7 +10,7 @@ def sms():
     incoming_message = request.forms.get('message')
     sender = request.forms.get('from')
     message = "New SMS received from "+sender+":\n"+incoming_message
-    application.bot.send_message(chat_id=config["chatId"], parse_mode=ParseMode.MARKDOWN_V2, text=message)
+    bot.send_message(chat_id=config["chatId"], parse_mode=ParseMode.MARKDOWN_V2, text=message)
     print(message)
     return HTTPResponse(status=200)
 
@@ -18,6 +19,5 @@ if __name__ == '__main__':
     _parser = configparser.ConfigParser()
     _parser.read("config.ini")
     config = _parser['DEFAULT']
-
-    application = ApplicationBuilder().token(config["BotToken"]).build()
+    bot = Bot(token=config["BotToken"])
     run(host='0.0.0.0', port=8080)
